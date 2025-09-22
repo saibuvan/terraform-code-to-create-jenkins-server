9d167d16a9488317c9c6ecb7232efded45209df6
Buvanesh <buvanesh.ganeshan@redingtongroup.com>
2025-09-22 11:49:19 +0530

    java version issue-1

--- jenkins-server-script.sh
+++ jenkins-server-script.sh
@@ -7,7 +7,7 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum upgrade -y
-sudo amazon-linux-extras install java-openjdk17 -y
+sudo amazon-linux-extras install openjdk-17-jdk -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins

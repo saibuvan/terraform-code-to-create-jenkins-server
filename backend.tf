3bfe5d585eb9e2f5af8291d03a1c8dac7394787c
Buvanesh <buvanesh.ganeshan@redingtongroup.com>
2025-09-22 11:19:59 +0530

    my project terraform3

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "jenkins-app-saibuvan"
+    bucket = "jenkins-app-saibuvan-1"
     region = "us-east-1"
     key = "jenkins-server/terraform.tfstate"
   }
